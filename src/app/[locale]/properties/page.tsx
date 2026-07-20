4d014b250514c7111f260a5f9d41163f94d5ffbc
romilvyas998-sudo <romilvyas998@gmail.com>
2026-07-20 13:40:01 +0530

    Fix Suspense issue

--- src/app/[locale]/properties/page.tsx
+++ src/app/[locale]/properties/page.tsx
@@ -1,3 +1,14 @@
-import { ListingsExperience } from '@/components/listings-experience';
+import { Suspense } from "react";
+import { ListingsExperience } from "@/components/listings-experience";
 
-export default function Properties({ params }: { params: { locale: string } }) { return <ListingsExperience locale={params.locale}/>; }
+export default function Properties({
+  params,
+}: {
+  params: { locale: string };
+}) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ListingsExperience locale={params.locale} />
+    </Suspense>
+  );
+}
